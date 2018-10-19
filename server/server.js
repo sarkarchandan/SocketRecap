2cf9531a71aff08d26d195dccc6abb228f6a105a
chandan <elitegreen@gmail.com>
2018-10-19 22:28:06 +0200

    Added connect and disconnect event controllers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000
+const socketIO = require('socket.io')
 
 const app = express()
+server = http.createServer(app)
+const io = socketIO(server);
+
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+io.on('connection',(socket) => {
+  console.log('Client connected')
+
+  socket.on('disconnect',(socket) => {
+    console.log('Client disconnected')
+  })
+})
+
+server.listen(port, () => {
   console.log('Server is running on ' + port)
 })
